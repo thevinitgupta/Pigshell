aa403e5e66d1cd491deb990054bbe6b0e7746317
Vinit Gupta <vinitman35@gmail.com>
2022-04-15 11:54:56 +0530

    add image upload and display functions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
+import ImageDisplay from './Components/ImageDisplay';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
         Pigshell
-      </header>
+      <ImageDisplay/>
     </div>
   );
 }
---
+++ src/Components/ImageDisplay.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import "../Css/ImageDisplay.css"
+
+function ImageDisplay() {
+    const [imageToConvert, setImageToConvert] = useState(null);
+    const [previewImage, setPreviewImage] = useState(null);
+
+    function fileChangedHandler(event){
+        setImageToConvert(event.target.files[0]);
+        setPreviewImage(URL.createObjectURL(event.target.files[0]));
+    }
+
+  return (
+    <div className='ImageDisplay'>
+    {/* <header>ImageDisplay</header> */}
+    <div className='ImageUploader'>
+    <input type="file" accept="image/*" name="uploadedImage" id="uploadedImage" onChange={fileChangedHandler}/>
+
+    {imageToConvert!=null && <div className='ImagePreview'>
+        <img src={previewImage} alt="to convert"/>
+    </div>}
+
+    </div>
+
+    </div>
+  )
+}
+
+export default ImageDisplay
