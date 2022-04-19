a44ebd48afda2d08fbd860b119ec1d86b940d6ad
thevinitgupta <thevinitgupta@gmail.com>
2022-04-19 18:51:09 +0530

    update loader

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import ImageDisplay from './Components/ImageDisplay';
+import Loader from "./Assets/Loader.svg"
 import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <img src={Loader} alt="pig loader"/>
       {/* <ImageDisplay/> */}
     </div>
   );
